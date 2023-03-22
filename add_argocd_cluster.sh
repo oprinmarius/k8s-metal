e0008fa9243c74fa9509af02b9bbee5154147247
Oprin Marius <moprin@cloudbasesolutions.com>
2023-03-22 11:43:44 +0200

    Change folder structure

--- add_argocd_cluster.sh
+++ add_argocd_cluster.sh
@@ -5,16 +5,19 @@
 
 # Get kubeconfig for kub-poc cluster
 clusterctl get kubeconfig kub-poc -n tink-system > ~/.kube/kub-poc.kubeconfig
-cat ~/.kube/kub-poc.kubeconfig | pbcopy
+
+# Copy kubeconfig to Argo CD server pod
+kubectl cp ~/.kube/kub-poc.kubeconfig -n argo-cd deploy/argo-cd-argocd-server -c server
 
 # Get shell into Argo CD server pod name
 kubectl exec -it -n argo-cd deploy/argo-cd-argocd-server -c server -- sh -c "clear; (bash || ash || sh)"
 
-# Create kubeconfig file
-cat <<EOF > kub-poc.kubeconfig
-<copy kubeconfig from clipboard>
-EOF
-
+# Argo CD login
+argocd login argo-cd.mgmt.kub-poc.local \
+   --username admin --password \
+   $(kubectl get secret argocd-initial-admin-secret -n argo-cd -o jsonpath="{.data.password}" | base64 -d) \
+   --insecure
+   
 # Add cluster to Argo CD
 argocd cluster add kub-poc-admin@kub-poc \
    --kubeconfig ./kub-poc.kubeconfig \
